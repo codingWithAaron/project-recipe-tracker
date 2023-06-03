1fbb3031e5dcb78dd6636ce5dcf940789f517f2f
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-06-03 16:08:25 -0600

    Updated the image element to have an alt property

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,7 +5,7 @@ function Recipe({recipe, deleteRecipe}){
         <tr>
             <td>{recipe.name}</td>
             <td>{recipe.cuisine}</td>
-            <td><img src={recipe.photo}></img></td>
+            <td><img src={recipe.photo} alt={recipe.name}></img></td>
             <td>{recipe.ingredients}</td>
             <td>{recipe.preparation}</td>
             <button name="delete" onClick={()=>{deleteRecipe(recipe)}}>Delete</button>
